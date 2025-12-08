eb8afa54a6f9a182a2afe4059d012f17c0b12cff
julien77777 <144126177+SideonD@users.noreply.github.com>
2025-12-08 10:26:40 +0100

    oui

--- scripts/generation/BiomeResource.gd
+++ scripts/generation/BiomeResource.gd
@@ -26,4 +26,3 @@ enum ZoneShape {
 @export var decoration_scenes: Array[PackedScene] = []
 # @export_range(0.0, 1.0) var decoration_density: float = 0.05 # Deprecated
 @export var decoration_scene_count_range: Vector2i = Vector2i(0, 2)
-

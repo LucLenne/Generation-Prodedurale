45d677958cb4d01aa48c7b30d325a3e9c8231d48
Shahine Benthanane <shahine93300@gmail.com>
2025-12-11 11:27:50 +0100

    new dialogue

--- scripts/EpitaphReader.gd
+++ scripts/EpitaphReader.gd
@@ -2,25 +2,57 @@ extends Node2D
 
 
 @onready var popup_position: Node2D = $PopupPosition
+@onready var interractable: Interractable = $Interractable
+
+const EPITAPH_FOLDER_PATH = "res://Assets/Epitaph/"
+
 
 @export var EpitaphJson : JSON
 var EpitaphText : String
 
 func _ready():
+	interractable.showLabel = true
+	load_random_json_files()
+	
 	var rulesFR = EpitaphJson.data
 	var grammarFR = TraceryFR.GrammarFR.new(rulesFR)
 
 	grammarFR.add_modifiers(TraceryFR.UniversalModifiersFR.get_modifiers())
-	
-	print("--- Tests Tracery FR ---")
-	
 	EpitaphText = grammarFR.flatten("#origin#")
-	print("Phrase 1 : " + EpitaphText)
 	
-
 
 func _on_interractable_on_player_interract() -> void:
 	var lines = EpitaphText.split("&&")
 	PopupManager.start_dialog(popup_position.global_position, lines)
+	
+
+func load_random_json_files():
+	var epitaph_files = _get_json_files_in_folder(EPITAPH_FOLDER_PATH)
+	if epitaph_files.size() > 0:
+		var random_epitaph_file = epitaph_files.pick_random()
+		EpitaphJson = load(random_epitaph_file)
+		print("Fichier Epitah chargé : " + random_epitaph_file)
+	else:
+		push_error("Aucun fichier JSON trouvé dans : " + EPITAPH_FOLDER_PATH)
+
+
+func _get_json_files_in_folder(path: String) -> Array[String]:
+	var files: Array[String] = []
+	var dir = DirAccess.open(path)
+	
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		
+		while file_name != "":
+			if not dir.current_is_dir() and file_name.ends_with(".json"):
+				files.append(path + "/" + file_name)
+			
+			file_name = dir.get_next()
+	else:
+		push_error("Impossible d'ouvrir le dossier : " + path)
+	
+	return files
+
 	
 	
